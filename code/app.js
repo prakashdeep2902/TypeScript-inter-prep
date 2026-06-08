0817a809b70ba87507f22c5eeaced775eee18576
sharmaJi <prakashdeep2902@gmail.com>
2026-06-08 18:06:16 +0530

    added first code

---
+++ code/app.js
@@ -0,0 +1,6 @@
+var companyName = "AceSales";
+var employees = 50;
+var isActive = true;
+console.log(companyName);
+console.log(employees);
+console.log(isActive);
---
+++ code/app.ts
@@ -0,0 +1,7 @@
+let companyName: string = "AceSales";
+let employees: number = 50;
+let isActive: boolean = true;
+
+console.log(companyName);
+console.log(employees);
+console.log(isActive);
